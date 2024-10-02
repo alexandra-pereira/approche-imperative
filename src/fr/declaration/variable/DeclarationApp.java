433807acb379379f4e5d38b5cb7d5dfe105bf3eb
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-02 15:25:17 +0200

    tp 02 variables declaration

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 22;
+        short b = -2000;
+        int c = 456399;
+        long d = 76_859_999_089L;
+        float e = -5.6F;
+        double f = 6758.89;
+        char g = 'v';
+        boolean h = false;
+        String i = "Hello";
+
+        System.out.println(g);
+
+        String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
