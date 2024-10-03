41b9d41c49756235c220cde0b05e6390e1378170
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-03 11:10:36 +0200

    tp 05 tableau entier

--- src/fr/boucles/ExerciceTableauEntier.java
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -3,5 +3,21 @@ package fr.boucles;
 public class ExerciceTableauEntier {
     public static void main(String[] args) {
 
+        // Création d'un tableau avec une taille de 10
+        int[] entiers = new int[10];
+
+        // Ajout des nombres de 1 à 10 dans le tableau
+        for (int i = 0; i < entiers.length; i++) {
+            entiers[i] = i + 1;
+        }
+
+        System.out.println("Premier élément du tableau: " + entiers[0] + "\n");
+
+        System.out.println("Longueur du tableau: " + entiers.length + "\n");
+
+        System.out.println("Dernier élément du tableau: " + entiers[entiers.length - 1] + "\n");
+
+        entiers[4] = 8;
+        System.out.println("Nouvelle valeur à l’index 4: " + entiers[4]);
     }
 }
