d3f830f133f916c156466b24e70734428d955376
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-03 16:57:10 +0200

    tp autonomie exercice recherche min

---
+++ src/fr/algorithmie/RechercheMin.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class RechercheMin {
+    public static void main(String[] args) {
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        // Rechercher le plus petit élément du tableau
+        int min = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+
+        System.out.println("Le plus petit élément du tableau est : " + min);
+    }
+}
+
