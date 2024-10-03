434f78ca53052ecfc974f229b9fc628031ccc238
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-03 16:54:21 +0200

    tp autonomie exercice recherche max + indentation dans affichage partiel  et inversion de contenu

--- src/fr/algorithmie/InversionContenu.java
+++ src/fr/algorithmie/InversionContenu.java
@@ -3,7 +3,7 @@ package fr.algorithmie;
 public class InversionContenu {
     public static void main(String[] args) {
 
-        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
         int lenght = array.length;
 
---
+++ src/fr/algorithmie/RechercheMax.java
@@ -0,0 +1,19 @@
+package fr.algorithmie;
+
+public class RechercheMax {
+    public static void main(String[] args) {
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        // Rechercher le plus grand élément du tableau
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        System.out.println("Le plus grand élément du tableau est : " + max);
+    }
+}
