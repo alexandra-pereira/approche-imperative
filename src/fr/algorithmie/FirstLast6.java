298812fb4a046f1d06a43ba17a0827b55d8e757b
alexandra-pereira <xandrapereira@hotmail.com>
2024-10-04 18:17:16 +0200

    tp autonomie first last 6

---
+++ src/fr/algorithmie/FirstLast6.java
@@ -0,0 +1,16 @@
+package fr.algorithmie;
+
+public class FirstLast6 {
+    public static void main(String[] args) {
+
+        int[] array = {6, 1, 2, 3};
+
+        boolean hasFirstOrLast6 = (array.length > 0) && (array[0] == 6 || array[array.length - 1] == 6);
+
+        if (hasFirstOrLast6) {
+            System.out.println("Le tableau a 6 au premier ou au dernier élément.");
+        } else {
+            System.out.println("Le tableau n'a pas 6 au premier ou au dernier élément.");
+        }
+    }
+}
